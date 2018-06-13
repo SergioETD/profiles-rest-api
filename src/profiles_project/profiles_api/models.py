77eab0df92348c7ba8ea5ca2dc8d1fbd909249d8
SergioETD <sergio.tendom95@gmail.com>
2018-06-13 17:09:49 -0500

    Added a custom user profile  model, a manager, and the migration for our database

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Helps Django work with our new custom user model."""
+
+    def create_user(self, email, name, password=None):
+        """Creates a new user profile object"""
+
+        if not email:
+            raise ValueError('Users must have an email')
+
+        email = self.normalize_email(email) #Normaliza la cadena de email, es decir, pone todo en minusculas
+        user = self.model(email = email, name = name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user #Retorna el objeto
+
+
+    def create_superuser(self, email, name, password):
+        """Creates and saves a new superuser with given details"""
+
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self._db)
+
+        return user
+
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Respents a user profile inside our system"""
+
+    email = models.EmailField(max_length=255, unique=True) #Captura el correo
+    name = models.CharField(max_length=255) #Captura el nombre
+    is_activate = models.BooleanField(default=True) #Determina si el usuario está actualmente activo
+    is_staff = models.BooleanField(default=False) #Para impedir que lleguen nuevos usuarios como autorizados
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+
+    """Required fields"""
+
+    REQUIRED_FIELDS = ['name'] #Email ya es requerido en el field, por eso no se coloca aquí
+
+    def get_full_name():  # "def" define una función
+        """Used to get a users full name"""
+        return self.name
+
+    def get_short_name(self):
+        """Used to get a users short name"""
+        return self.name
+
+    def __str__(self):
+        """Django uses this when it needs to convert the object to a string"""
+        return self.email
--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -121,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile' #Agregar esta entrada para indicar/asignar a django que debe usar este modelo
